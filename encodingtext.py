2743d9c0c896fc168c3e2cc306fdd0b774168415
Suyeon-WOO1 <125536266+Suyeon-WOO1@users.noreply.github.com>
2024-06-26 14:53:04 +0900

    Update encodingtext.py

--- encodingtext.py
+++ encodingtext.py
@@ -1,5 +1,7 @@
-/*
-
+'''
 encoding tokens using one-hot encoding
+'''
 
-*/
+import tensorflow as tf
+from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
